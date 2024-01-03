f0a3c6f14be752c84b24018b81ce88030b53b823
Rajdip27 <srajdip920@gmail.com>
2024-01-03 13:32:24 +0600

    Create Table And View Fix

---
+++ FlyNest.Infrastructure/Migrations/20240103060801_CreateStoppies.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlyNest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateStoppies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stoppies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AirportId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    ModifiedBy = table.Column<long>(type: "bigint", nullable: true),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateNo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stoppies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stoppies_Airport_AirportId",
+                        column: x => x.AirportId,
+                        principalTable: "Airport",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stoppies_AirportId",
+                table: "Stoppies",
+                column: "AirportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stoppies");
+        }
+    }
+}
