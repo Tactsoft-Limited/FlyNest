b60d2f29baa72782f84c58f0420e2add4d19fc8f
Rajdip27 <srajdip920@gmail.com>
2024-01-14 18:00:11 +0600

    new outbound has added for nepal

---
+++ FlyNest.App/Controllers/OutboundController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlyNest.App.Controllers;
+
+public class OutboundController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Nepal()
+    {
+        return View();
+    }
+}
