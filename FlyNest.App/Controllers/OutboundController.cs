a63c33802f756645e95b3dec35bcc47e67bba0e0
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2024-01-15 11:07:12 +0600

    outbound nepal page has added

--- FlyNest.App/Controllers/OutboundController.cs
+++ FlyNest.App/Controllers/OutboundController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FlyNest.App.Controllers;
 
+[AllowAnonymous]
 public class OutboundController : Controller
 {
     public IActionResult Index()
