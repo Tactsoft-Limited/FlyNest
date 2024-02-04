f4d92a0c2514012a93405766a477b3a6c8e851c4
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2024-02-04 12:14:18 +0600

    nepal tour package has added

---
+++ FlyNest.App/Controllers/HolidayController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyNest.App.Controllers
+{
+    [AllowAnonymous]
+    public class HolidayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult International()
+        {
+            return View();
+        }
+        public IActionResult Nepal()
+        {
+            return View();
+        }
+    }
+}
