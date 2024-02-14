8d941e71be8a4a6f1e6d5191564e1d28d2b2dcb3
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2024-02-14 15:05:57 +0600

    visa processing page design completed.

---
+++ FlyNest.App/Controllers/VisaProcessingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyNest.App.Controllers
+{
+    [AllowAnonymous]
+    public class VisaProcessingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
