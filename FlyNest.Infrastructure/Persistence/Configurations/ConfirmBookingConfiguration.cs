b888c80daa45862beea02d3005beff178309e6e7
khpolash <khpolashbd@gmail.com>
2024-02-24 12:33:56 +0600

    Add confirmboking table

---
+++ FlyNest.Infrastructure/Persistence/Configurations/ConfirmBookingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using FlyNest.SharedKernel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlyNest.Infrastructure.Persistence.Configurations;
+
+public class ConfirmBookingConfiguration : IEntityTypeConfiguration<ConfirmBooking>
+{
+    public void Configure(EntityTypeBuilder<ConfirmBooking> builder)
+    {
+        builder.ToTable(nameof(ConfirmBooking));
+        builder.HasKey(x => x.Id);
+        builder.HasOne(x => x.TourPackage).WithMany(x => x.ConfirmBookings).HasForeignKey(x => x.TourPackageId);
+        builder.Property(x => x.PackageTitle).HasMaxLength(150);
+        builder.Property(x => x.TransactionId).HasMaxLength(85);
+        builder.Property(x => x.ClientName).HasMaxLength(85);
+        builder.Property(x => x.ClientEmail).HasMaxLength(85);
+        builder.Property(x => x.ClientPhone).HasMaxLength(85);
+    }
+}
---
+++ FlyNest.SharedKernel/Entities/ConfirmBooking.cs
@@ -0,0 +1,21 @@
+﻿using FlyNest.SharedKernel.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlyNest.SharedKernel.Entities;
+
+public class ConfirmBooking : AuditableEntity
+{
+    public long TourPackageId { get; set; }
+    public TourPackage TourPackage { get; set; }
+    public double PackagePrice { get; set; }
+    public string PackageTitle { get; set; }
+    public string TransactionId { get; set; }
+    public int TotalPerson { get; set; }
+    public double TotalAmount { get; set; }
+    public string ClientName { get; set; }
+    public string ClientEmail { get; set; }
+    public string ClientPhone { get; set; }
+    public string PaymentMethod { get; set; }
+    public bool TramAndCondition { get; set; }
+}
