d2f8547f3c2af9d890dd76453f8080c58fb13c49
khpolash <khpolashbd@gmail.com>
2024-04-23 17:45:14 +0600

    add visa request repository

---
+++ FlyNest.Application/Repositories/Entities/VisaRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using FlyNest.Application.Interfaces.Entities;
+using FlyNest.Application.Repositories.BaseRepo;
+using FlyNest.Application.ViewModels.VmEntities;
+using FlyNest.Infrastructure.Persistence;
+using FlyNest.SharedKernel.Entities;
+
+namespace FlyNest.Application.Repositories.Entities;
+
+public class VisaRequestRepository(FlyNestDbContext context) : BaseRepository<VisaRequest>(context), IVisaRequestRepository
+{
+}
