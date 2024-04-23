14d98cd12d57a0559888a764dd2f36773295e6a0
khpolash <khpolashbd@gmail.com>
2024-04-23 17:49:02 +0600

    property update

--- FlyNest.Application/ViewModels/VmEntities/Search/VmReservation.cs
+++ FlyNest.Application/ViewModels/VmEntities/Search/VmReservation.cs
@@ -1,12 +1,19 @@
-﻿namespace FlyNest.Application.ViewModels.VmEntities.Search;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FlyNest.Application.ViewModels.VmEntities.Search;
 
 public class VmReservation
 {
     public VmHotelReservation HotelReservation { get; set; }
-
     public VmFlightReservation FlightReservation { get; set; }
+
+    public VmVisaProcessRequest VmVisaProcessRequest { get; set; }
+    public VmVisaRequirement VmVisaRequirement { get; set; }
 
     public List<VmImageSlider> ImageSlider { get; set; }
     public List<VmCountry> CountryList { get; set; }
     public List<VmTourPackage> ExploreBDList { get; set; }
+
+    public IEnumerable<SelectListItem> CountryDropdown { get; set; }
+
 }
