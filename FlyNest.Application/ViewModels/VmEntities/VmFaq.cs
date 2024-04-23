06f44b3e705fa8c4b144e9c4b8be12d5e51f23f8
khpolash <khpolashbd@gmail.com>
2024-04-23 17:56:06 +0600

    add faq view model

---
+++ FlyNest.Application/ViewModels/VmEntities/VmFaq.cs
@@ -0,0 +1,19 @@
+﻿using FlyNest.SharedKernel.Entities.BaseEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlyNest.Application.ViewModels.VmEntities;
+
+public class VmFaq : BaseEntity
+{
+    [Required(ErrorMessage = "Category is required.")]
+    [StringLength(100, ErrorMessage = "Category length must be less than or equal to 100 characters.")]
+    public string Category { get; set; }
+
+    [Required(ErrorMessage = "Question is required.")]
+    [StringLength(300, ErrorMessage = "Question length must be less than or equal to 300 characters.")]
+    public string Question { get; set; }
+
+    [Required(ErrorMessage = "Answer is required.")]
+    [StringLength(1000, ErrorMessage = "Answer length must be less than or equal to 1000 characters.")]
+    public string Answer { get; set; }
+}
