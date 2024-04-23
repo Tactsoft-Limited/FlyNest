f191faba3423841da0dc139fc6e2f64166c6de11
khpolash <khpolashbd@gmail.com>
2024-04-23 17:35:42 +0600

    create faq model

---
+++ FlyNest.SharedKernel/Entities/Faq.cs
@@ -0,0 +1,10 @@
+﻿using FlyNest.SharedKernel.Entities.BaseEntities;
+
+namespace FlyNest.SharedKernel.Entities;
+
+public class Faq : AuditableEntity
+{
+    public string Category { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+}
