2719be147f100fca29883f206bbc7f11f059bb61
khpolash <khpolashbd@gmail.com>
2024-04-23 17:35:09 +0600

    add field

--- FlyNest.SharedKernel/Entities/VisaRequirement.cs
+++ FlyNest.SharedKernel/Entities/VisaRequirement.cs
@@ -4,6 +4,7 @@ namespace FlyNest.SharedKernel.Entities;
 
 public class VisaRequirement : AuditableEntity
 {
+    public string Title { get; set; }
     public long CountryId { get; set; }
     public Country Country { get; set; }
     public string BasicDocument { get; set; }
@@ -11,6 +12,8 @@ public class VisaRequirement : AuditableEntity
     public string ForJobHolder { get; set; }
     public string ForStudent { get; set; }
     public string ForMedical { get; set; }
+    public string Validity { get; set; }
+    public string MaxStay { get; set; }
     public string VisaFee { get; set; }
     public string Others { get; set; }
 }
