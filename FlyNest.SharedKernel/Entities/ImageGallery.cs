1b649282e0a6bd1d09b270f2417dd79ec7ed6859
khpolash <khpolashbd@gmail.com>
2024-04-23 17:35:30 +0600

    create image gallery model

---
+++ FlyNest.SharedKernel/Entities/ImageGallery.cs
@@ -0,0 +1,10 @@
+﻿using FlyNest.SharedKernel.Entities.BaseEntities;
+
+namespace FlyNest.SharedKernel.Entities;
+
+public class ImageGallery : AuditableEntity
+{
+    public string EventTitle { get; set; }
+    public DateTime EventDate { get; set; }
+    public string ImageUrl { get; set; }
+}
