46533e8f62a153c9b15c5550ba196e86bca1b4c5
khpolash <khpolashbd@gmail.com>
2024-04-23 17:42:58 +0600

    add field nullable

--- FlyNest.Infrastructure/Interfaces/BaseRepo/IBaseRepository.cs
+++ FlyNest.Infrastructure/Interfaces/BaseRepo/IBaseRepository.cs
@@ -12,13 +12,13 @@ public interface IBaseRepository<T> where T : class
 
     Task<List<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
 
-    T FirstOrDefault(long id);
+    T FirstOrDefault(long? id);
 
-    Task<T> FirstOrDefaultAsync(long id);
+    Task<T> FirstOrDefaultAsync(long? id);
 
-    T FirstOrDefault(long id, params Expression<Func<T, object>>[] includeProperties);
+    T FirstOrDefault(long? id, params Expression<Func<T, object>>[] includeProperties);
 
-    Task<T> FirstOrDefaultAsync(long id, params Expression<Func<T, object>>[] includeProperties);
+    Task<T> FirstOrDefaultAsync(long? id, params Expression<Func<T, object>>[] includeProperties);
 
     #region CURD Operation
     Task InsertAsync(T entity);
