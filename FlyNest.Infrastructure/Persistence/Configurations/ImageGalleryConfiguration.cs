e4677eaaf0273df2badbd07e400b08212b9f76e8
khpolash <khpolashbd@gmail.com>
2024-04-23 17:37:58 +0600

    configure image gallery model

---
+++ FlyNest.Infrastructure/Persistence/Configurations/ImageGalleryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using FlyNest.SharedKernel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlyNest.Infrastructure.Persistence.Configurations;
+
+public class ImageGalleryConfiguration : IEntityTypeConfiguration<ImageGallery>
+{
+    public void Configure(EntityTypeBuilder<ImageGallery> builder)
+    {
+        builder.ToTable(nameof(ImageGallery));
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.EventTitle).HasMaxLength(255);
+        builder.Property(x => x.ImageUrl).HasMaxLength(100);
+    }
+}
