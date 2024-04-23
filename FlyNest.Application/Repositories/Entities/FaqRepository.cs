d6529063c56224dd402d7efab3dac5589f6dfda0
khpolash <khpolashbd@gmail.com>
2024-04-23 17:45:49 +0600

    add faq repository

---
+++ FlyNest.Application/Repositories/Entities/FaqRepository.cs
@@ -0,0 +1,10 @@
+﻿using FlyNest.Application.Interfaces.Entities;
+using FlyNest.Application.Repositories.BaseRepo;
+using FlyNest.Infrastructure.Persistence;
+using FlyNest.SharedKernel.Entities;
+
+namespace FlyNest.Application.Repositories.Entities;
+
+public class FaqRepository(FlyNestDbContext context) : BaseRepository<Faq>(context), IFaqRepository
+{
+}
