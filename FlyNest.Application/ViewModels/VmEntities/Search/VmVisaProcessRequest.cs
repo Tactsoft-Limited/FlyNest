4402952be5d55a64810d39863950cfa60c3ef239
khpolash <khpolashbd@gmail.com>
2024-04-23 17:50:33 +0600

    visa process request model

---
+++ FlyNest.Application/ViewModels/VmEntities/Search/VmVisaProcessRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlyNest.Application.ViewModels.VmEntities.Search;
+
+public class VmVisaProcessRequest
+{
+    public long? CountryId { get; set; }
+    public double ProcessingFee { get; set; }
+    public double VisaFee { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime FromDate { get; set; } = DateTime.Now;
+
+    [DataType(DataType.Date)]
+    public DateTime ToDate { get; set; } = DateTime.Now;
+
+    public int TotalTraveller { get; set; } = 1;
+}
