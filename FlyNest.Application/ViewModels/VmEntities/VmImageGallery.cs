ccc7658d142e8f6805cd48b7f5023938fce05dd9
khpolash <khpolashbd@gmail.com>
2024-04-23 17:55:21 +0600

    add VmImageGallery view model

---
+++ FlyNest.Application/ViewModels/VmEntities/VmImageGallery.cs
@@ -0,0 +1,20 @@
+﻿using FlyNest.SharedKernel.Entities.BaseEntities;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlyNest.Application.ViewModels.VmEntities;
+
+public class VmImageGallery : BaseEntity
+{
+    [Display(Name = "Event Title")]
+    public string EventTitle { get; set; }
+
+    [Display(Name = "Event Date")]
+    [DataType(DataType.Date)]
+    public DateTime EventDate { get; set; } = DateTime.Now;
+
+    [Display(Name = "Image")]
+    public string ImageUrl { get; set; }
+
+    public IFormFile ImageUrlFile { get; set; }
+}
