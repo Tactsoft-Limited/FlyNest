df1f384cc7b6c23afea289751ec23d6c586b100d
khpolash <khpolashbd@gmail.com>
2024-04-23 17:25:15 +0600

    add visa request controller

---
+++ FlyNest.App/Controllers/Admin/VisaRequestController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using FlyNest.Application.Interfaces.Entities;
+using FlyNest.Application.ViewModels.VmEntities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyNest.App.Controllers.Admin;
+
+public class VisaRequestController(IVisaRequestRepository visaRequestRepository, IMapper mapper) : Controller
+{
+    private readonly IVisaRequestRepository _visaRequestRepository = visaRequestRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IActionResult> Index()
+    {
+        var requestList = await _visaRequestRepository.GetAllAsync();
+        return View(_mapper.Map<List<VmVisaRequest>>(requestList));
+    }
+
+    public async Task<IActionResult> Details(long id)
+    {
+        var request = await _visaRequestRepository.FirstOrDefaultAsync(id);
+        return View(_mapper.Map<VmVisaRequest>(request));
+    }
+}
