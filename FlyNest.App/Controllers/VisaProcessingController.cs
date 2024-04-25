d476e33d875170b670d4bc1167ccb9869eaca2fe
khpolash <khpolashbd@gmail.com>
2024-04-25 15:46:44 +0600

    add index method

--- FlyNest.App/Controllers/VisaProcessingController.cs
+++ FlyNest.App/Controllers/VisaProcessingController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using FlyNest.Application.Interfaces.Entities;
+using FlyNest.Application.ViewModels.VmEntities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlyNest.App.Controllers;
 
 [AllowAnonymous]
-public class VisaProcessingController : Controller
+public class VisaProcessingController(IVisaRequirementRepository visaRequirementRepository, IMapper mapper) : Controller
 {
-    public IActionResult Index()
+    private readonly IVisaRequirementRepository _visaRequirementRepository = visaRequirementRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var visaRequrementList = await _visaRequirementRepository.GetAllAsync(x => x.Country);
+        return View(_mapper.Map<List<VmVisaRequirement>>(visaRequrementList));
     }
 }
