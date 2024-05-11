3f35280214d43c2dee40591570782d8aa1609cb7
khpolash <khpolashbd@gmail.com>
2024-05-11 17:18:54 +0600

    change directory

---
+++ FlyNest.Infrastructure/Persistence/Configurations/PolicyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using FlyNest.SharedKernel.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlyNest.Infrastructure.Persistence.Configurations;
+
+public class PolicyConfiguration : IEntityTypeConfiguration<Policy>
+{
+    public void Configure(EntityTypeBuilder<Policy> builder)
+    {
+        builder.ToTable(nameof(Policy));
+        builder.HasKey(x => x.Id);
+        builder.HasOne(x => x.Flight).WithMany(x => x.Policies).HasForeignKey(x => x.FlightId);
+
+    }
+}
